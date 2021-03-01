2cfcd6bbce678b5d70fc25871c8da1141c5a1452
Radiyah <rsabur58@gmail.com>
2021-03-01 18:54:28 -0500

    commit tree 43f0abf696550998af3f07436a61b605594ee95a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+# binding.pry
+    attr_accessor :word
+
+    @@word = []
+
+    def initialize(word)
+        @word = word
+    end
+
+    def self.match(word)
+        word.select{|word| word == word}
+    end
+end
